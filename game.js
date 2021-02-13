38ff645cdc2361ee24f49f31853ea22bda526981
Angelo Nastro <58071991+AngeloNastro@users.noreply.github.com>
2021-02-13 12:11:45 +0000

    initial commit

---
+++ game.js
@@ -0,0 +1,42 @@
+import {update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection, SNAKE_SPEED} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board')
+
+//Game Loop
+function main(currentTime){
+    if(gameOver){
+       if(confirm('You lost. Presso ok to restart.')){
+           window.location = '/';
+       }
+       return
+    }
+    window.requestAnimationFrame(main);
+    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondSinceLastRender < 1 /SNAKE_SPEED) return
+    lastRenderTime = currentTime;
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+function draw(){
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard)  
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
